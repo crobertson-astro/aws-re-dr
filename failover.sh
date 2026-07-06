d7648746ab899cb77b75f788c71e7fb0c097b1b7
crobertson-astro <chase.robertson@astronomer.io>
2026-07-06 08:41:08 -0700

    drain before scale down

--- failover.sh
+++ failover.sh
@@ -7,6 +7,10 @@ kubectl scale deployment --all --replicas=1
 
 kubectl config use-context primary
 echo "Scaling down primary deployments..."
+# In a real failover scenario, failover will be unavailable so these cmds will likely fail.
+NODE_NAME=$(kubectl get nodes -o jsonpath='{.items[0].metadata.name}')
+kubectl drain $NODE_NAME
 kubectl scale deployment --all --replicas=0
 
+# reset context to failover so that the user can continue working with the failover cluster
 kubectl config use-context failover
--- fallback-to-primary.sh
+++ fallback-to-primary.sh
@@ -7,6 +7,10 @@ kubectl scale deployment --all --replicas=1
 
 kubectl config use-context failover
 echo "Scaling down failover deployments..."
+# Drain the failover node to prevent new pods from being scheduled there, then scale down.
+NODE_NAME=$(kubectl get nodes -o jsonpath='{.items[0].metadata.name}')
+kubectl drain $NODE_NAME
 kubectl scale deployment --all --replicas=0
 
+# reset context to primary so that the user can continue working with the primary cluster
 kubectl config use-context primary
